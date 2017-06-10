2c54352e936ab1ff7bbd455f355c212e1e4e47a4
shencanhui <shencanhui@foxmail.com>
2017-06-10 19:10:24 +0800

    trans chinese to english

--- Exp_3/Alarm.cc
+++ Exp_3/Alarm.cc
@@ -15,8 +15,8 @@ Alarm::~Alarm()
 void 
 check(int  which)
 {
-	//反复检查当前有几个等待闹钟的线程，若为零则结束，若不为零则切换，
-	//静态变量
+	// check how many thread in waiting, if zero ,exit,else switch 
+	// using static var
 	while(Alarm::num != 0){
 		currentThread->Yield();
 	}
